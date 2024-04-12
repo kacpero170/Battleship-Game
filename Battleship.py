c9ef36e2fdb7d86eb2d33e8db6be5eaa4adef18a
Kacper Staroń <164014637+kacpero170@users.noreply.github.com>
2024-04-12 17:22:28 +0200

    Update Battleship.py

--- Battleship.py
+++ Battleship.py
@@ -1,19 +1,21 @@
 from random import randint
-print("The indexes of rows and columns are 0-4.")
+print("")
+print("The numbers of rows and columns are 0-4.")
 board = []
 
-for x in range(0, 5):
-  board.append(["O"] * 5)
+size = 5
+for x in range(0, size):
+  board.append(["O"] * size)
 
 def print_board(board):
   for row in board:
     print(" ".join(row))
-
+print("")
 print_board(board)
+print("")
 
 def random_row(board):
   return randint(0, len(board) - 1)
-
 def random_col(board):
   return randint(0, len(board[0]) - 1)
 
@@ -22,22 +24,24 @@ ship_col = random_col(board)
 
 for turn in range(4):
   print("Turn", turn + 1)
-  guess_row = int(input("Guess Row: "))
-  guess_col = int(input("Guess Col: "))
+  guess_row = int(input("Guess the row: "))
+  guess_col = int(input("Guess the column: "))
 
   if guess_row == ship_row and guess_col == ship_col:
     print("Congratulations! You sank my battleship!")
-    break  
+    break
   else:
-    if guess_row not in range(5) or \
-      guess_col not in range(5):
+    if guess_row not in range(size) or \
+      guess_col not in range(size):
       print("Oops, that's not even in the ocean.")
     elif board[guess_row][guess_col] == "X":
       print("You guessed that one already.")
     else:
       print("You missed my battleship!")
       board[guess_row][guess_col] = "X"
+    print("")
     print_board(board)
+    print("")
   
     if turn == 3:
-      print("Game Over")
+      print("Game Over.")
